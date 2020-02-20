3f8c7f20e6228debbd2d9c4a9fe4b679a1cdee31
NY <nyssance@icloud.com>
2020-02-21 00:00:01 +0800

    fix

--- Genos/Sources/Navigator.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Copyright © 2019 NY <nyssance@icloud.com>. All rights reserved.
-//
-
-public struct Navigator {
-    public static func routes(_ controller: UIViewController.Type, path: String) {}
-}
